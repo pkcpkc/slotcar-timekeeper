30d0252faf9807255bd530a95d93173c7358e522
Paul Hackenberger <paul.hackenberger@gmail.com>
2023-01-05 15:12:12 +0100

    Smaller video and more time data

--- timekeeper.js
+++ timekeeper.js
@@ -8,15 +8,19 @@ let timetable = getTimetableFields();
 
 function getTimetableFields() {
     let laps = document.querySelectorAll('#timekeeper .laps');
-    let bestLap = document.querySelectorAll('#timekeeper .bestLap');
+    let bestLaps = document.querySelectorAll('#timekeeper .bestLap');
     let lapTimes = document.querySelectorAll('#timekeeper .lapTimes');
+    let totalTimes = document.querySelectorAll('#timekeeper .totalTime');
+    let lapAverages = document.querySelectorAll('#timekeeper .lapAverage');
 
     let timetable = [];
     for (var i = 0; i < laps.length; i++) {
         timetable.push({
             'laps': laps[i],
-            'bestLap': bestLap[i],
-            'lapTimes': lapTimes[i]
+            'totalTime': totalTimes[i],
+            'bestLap': bestLaps[i],
+            'lapTimes': lapTimes[i],
+            'lapAverage': lapAverages[i]
         });
     }
     return timetable;
@@ -38,17 +42,22 @@ function detectCar(carId) {
 
     timekeeper[carId].push(now);
     let li = document.createElement('li');
-    li.innerHTML = formatMillis(timeDifference);
+    li.innerHTML = `<b>${formatMillis(timeDifference)}</b>`;
     timetable[carId].lapTimes.appendChild(li);
 
     var fastestTime = Number.MAX_VALUE;
+    var totalTime = 0;
     for (var i = 1; i < timekeeper[carId].length; i++) {
         let diff = timekeeper[carId][i] - timekeeper[carId][i - 1];
         if (diff < fastestTime) {
             fastestTime = diff;
         }
+        totalTime += diff;
     }
 
-    timetable[carId].laps.innerHTML = timekeeper[carId].length - 1;
+    let laps = timekeeper[carId].length - 1;
+    timetable[carId].laps.innerHTML = laps;
     timetable[carId].bestLap.innerHTML = formatMillis(fastestTime);
+    timetable[carId].totalTime.innerHTML = formatMillis(totalTime);
+    timetable[carId].lapAverage.innerHTML = formatMillis(Math.floor(totalTime / laps));
 }
